ccafa502927258970e027da52760fc99e444027b
sara-ghazy <saraghazy434@gmail.com>
2022-04-06 02:44:21 +0200

    Auth

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\ProductController;
 use App\Http\Controllers\AdminCategoryController;
+use App\Http\Controllers\Api\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,6 +21,10 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::post("/register",[ AuthController::class, 'register']);
+Route::post("/login", [AuthController::class,'login']);
+
 Route::resource('/categories', CategoryController::class);
+
 Route::resource('products',ProductController::class);
 Route::get('getproducts/{id}',[ProductController::class,'getProductsbyCategory']);
