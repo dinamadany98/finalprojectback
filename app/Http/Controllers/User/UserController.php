4e43a34ba05ec324515f382573e3e6ae5d318620
Dina Madany <dodyymadany98@gmail.com>
2022-04-09 22:22:44 +0200

    show user profile

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -7,7 +7,12 @@ use Illuminate\Http\Request;
 use App\Models\User;
 class UserController extends Controller
 {
-
+    public function show($userid)
+    {
+        $user=User::find($userid);
+        return $user;
+    
+    }
     public function update(Request $request, User $user)
     {
         $user->update($request->all());
