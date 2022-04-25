5986046503d7d868b4847c288eeafd475216c216
Dina Madany <dodyymadany98@gmail.com>
2022-04-25 14:07:32 +0200

    user

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -26,7 +26,7 @@ class AuthController extends Controller
             'name'=>$input["name"],
             'email'=>$input["email"],
             'password' =>Hash::make($input["password"]),
-            // 'role'=>$input['role']
+             'role'=>$input['role']
 
 
         ]);
